07b75e8ff5fe26eac98047cec009901e4c9c93a5
Valeria Radchenko <leraradchenko818@gmail.com>
2023-06-13 01:56:01 +0300

    add game logic

--- routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const authController = require('../controllers/auth.controller');
-
-router.post('/login', authController.loginAdmin);
-router.post('/register', authController.adminRegister);
-
-module.exports = router;
--- routes/game.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const gameController = require('../controllers/game.controller');
-const adminMiddleware = require('../middleware/auth.middleware');
-
-router.get('/', adminMiddleware, gameController.getListGames);
-router.delete('/list', adminMiddleware, gameController.deleteMany);
-
-module.exports = router;
